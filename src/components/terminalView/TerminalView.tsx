b54eacd9488603126747f4d66cf510c03fec9570
Felix <fedecraft3312@gmail.com>
2024-04-18 15:22:48 -0300

    .

--- src/components/terminalView/TerminalView.tsx
+++ src/components/terminalView/TerminalView.tsx
@@ -1,13 +1,17 @@
 import Anser from "anser";
 
+import styles from "./terminalView.module.css";
+
 /**
  * Show app output
  */
 export default function TerminalView({ output }: { output: string }) {
     
     return (
-        <div className="bg-gray-950 border rounded border-gray-100 text-slate-50 p-1 m-1" hidden={!output}>
-            <pre dangerouslySetInnerHTML={{ __html: Anser.ansiToHtml(output) }}></pre>
+        <div className={`${styles.view}`} hidden={!output}>
+            <div className={`bg-gray-950 border rounded border-gray-100 text-slate-50 p-1 m-1 ${styles.outputContainer}`}>
+                <pre dangerouslySetInnerHTML={{ __html: Anser.ansiToHtml(output) }} className={`${styles.output}`} style={{overflowWrap: "break-word"}}></pre>
+            </div>
         </div>
     );
 }
