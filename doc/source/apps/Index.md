4c356bfe193624f1b5ee195a001e840162b6c32f
Felix <fedecraft3312@gmail.com>
2024-04-26 09:17:22 -0300

    Update pid on the database

--- doc/source/apps/Index.md
+++ doc/source/apps/Index.md
@@ -6,6 +6,8 @@ App management
 
 # TODO
 
+Which ones are useful and which ones aren't is up to you
+
 - [x] Fetch apps
 - [x] App actions
 - [x] Update app output through web sockets
@@ -20,6 +22,10 @@ App management
     - [ ] Filter apps that are running
     - [ ] Filter apps with output
     - [ ] Filter apps by keywords
+- [ ] View
+    - [ ] View apps by folder name
+    - [ ] View apps by display name(packageJson.displayName)
+    - [ ] View apps by package name
 - [ ] Be able to group apps together
 
 Mainly for 'good-roots' stack
--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,47 @@
-// Here we can setup environment variables
-// TODO: Call some backend to set environment variables 
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // /** @type {import('next').NextConfig} */
 const nextConfig = {
 };
+
+// The package is not working 😡😡😡😡
+// I've to do this for every nextjs package
+/**
+ * Update app information on the database
+ */
+function updateAppInfo() {
+    const raw = fs.readFileSync(path.join(__dirname, "package.json"));
+    const packageJson = JSON.parse(raw.toString());
+    
+    // Insert / Update process to the database
+    const url = `http://localhost:24000`;
+    const headers = {
+        "Content-Type": "application/json"
+    };
+    const data = {
+        name: packageJson.name,
+        pid: process.pid,
+        appType: "application",
+        url: ``,
+    };
+    
+    fetch(`${url}/process`, {
+        method: "POST",
+        headers,
+        body: JSON.stringify(data)
+    }).then((res) => {
+        // console.log(`Response body: `, res.body);
+    }).catch((err) => {
+        console.error(`Error: `, err);
+    });
+}
+
+updateAppInfo();
 
 export default nextConfig;
