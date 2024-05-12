04d9f6996474045cb1996e07114e407ac078c5d5
Felix <fedecraft3312@gmail.com>
2024-05-12 09:39:23 -0300

    General frontend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,18 +6,20 @@ import Link from "next/link";
  * @returns 
  */
 export default function Home() {
+    const titleClasses = "m-1 p-1 text-xs font-extrabold leading-none tracking-tight text-gray-900 md:text-2xl lg:text-3xl";
+    
     return (
         <div>
-            <h1>Home</h1>
+            <h1 className={titleClasses}>Home</h1>
             
             {/* Links */}
-            <h2>
+            <h2 className={titleClasses}>
                 Links
             </h2>
             <nav>
                 <ul>
                     <li>
-                        <Link href="/doc">Documentation</Link>
+                        <Link href="/doc" className={"p-1 m-1 font-medium text-blue-600 dark:text-blue-500 hover:underline"}>Documentation</Link>
                     </li>
                 </ul>
             </nav>
